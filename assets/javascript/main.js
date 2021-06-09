5f64816e7b966736efa6708ae13b616f195b5b47
Nathaniel091 <nathaniel091@gmail.com>
2021-06-09 09:35:57 +0100

    done with formToggle. Next is to effect the send message action probably with Email.js

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,10 +1,40 @@
 
-// let formToggleBtn = git document.querySelector("form-toggle-btn");
-let formToggleBtn = document.getElementById("form-toggle-btn");
-// sendMessageBtn.addEventListener('click', openSendMessageForm);
+const formContainer = document.querySelector("#form-container");
 
-// function openSendMessageForm() {
-// 	console.log(sendMessageBtn)
-// }
+const formToggleBtn = document.querySelector("#form-toggle-btn");
+formToggleBtn.addEventListener('click', function(event){
+	event.preventDefault();
+	launchSendMessageForm();
+});
+
+const formCloseBtn = document.querySelector("form #close-btn");
+formCloseBtn.addEventListener('click', function(event){
+	event.preventDefault();
+	closeSendMessageForm();
+});
+
+// function 'launchSendMessageForm'
+function launchSendMessageForm() {
+	/*background: rgba(101, 119, 134, 0.9);*/
+	formContainer.classList.remove('d-none');
+	const formWrapper = document.querySelector("#form-container div");
+	// formWrapper.style.backgroundColor = 'transparent';
+	formWrapper.style.backgroundColor = 'rgba(101, 119, 134, 0.9)';
+	formWrapper.style.width = '100%';
+	// console.log(formWrapper)
+}
+
+// function 'closeSendMessageForm'
+function closeSendMessageForm() {
+	formContainer.classList.add('d-none');
+}
+
 // console.log(openSendMessageForm())
-console.log(sendMessageBtn)
+// console.log(formToggleBtn)
+
+
+
+
+
+
+
