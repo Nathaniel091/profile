da37ab3222171b5a6f6398f1adff214c910bda00
Nathaniel091 <nathaniel091@gmail.com>
2021-06-12 23:01:57 +0100

    fixed the notification-box, next is the form errors

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,7 +10,7 @@ notificationCloseBtn.addEventListener('click', closeNotificationBox);
 function openNotificationBox() {
 	notificationBox.style.top = '60px';
 	notificationBox.classList.remove('d-none');
-	// setTimeout(closeNotificationBox, 3000);
+	setTimeout(closeNotificationBox, 3000);
 }
 
 // function 'closeNotificationBox'
@@ -18,7 +18,7 @@ function closeNotificationBox() {
 	notificationBox.classList.add('d-none');
 }
 
-// openNotificationBox()
+openNotificationBox()
 
 
 /*--------------------------------------------------------------
